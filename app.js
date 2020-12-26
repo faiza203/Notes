14739be6aca6ef97c453c87d27adcadfee625df6
faiza203 <fiazfaiza981@gmail.com>
2020-12-26 00:16:03 -0800

    Add function to detect command and give response

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const yargs = require("yargs");
 
 const argv = yargs.argv;
 
-const command = argv._.length > 0 ? yargs.argv._[0] : undefined;
-console.log(command);
+const command = argv._.length > 0 ? yargs.argv._[0].toUpperCase() : undefined;
+
+if(command){
+    if(command === "ADD") console.log("Note is added")
+    else if(command === "READ") console.log("Your note is here") 
+    else if(command === "LIST") console.log("Notes are following") 
+    else if(command === "REMOVE")console.log("Note is removed") 
+    else console.log(`Your command ${yargs.argv._[0]} not recognize`) 
+}
