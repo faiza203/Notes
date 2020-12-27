dc5358c3482a2dc3a9fc859062d113d5341261e1
faiza203 <fiazfaiza981@gmail.com>
2020-12-27 11:45:57 -0500

    Complete

--- app.js
+++ app.js
@@ -1,60 +1,63 @@
 const yargs = require("yargs");
 
-const titleOptions = { 
-    describe : "Show title",
-    demand :true,
+const titleOptions = {
+    describe: "Show title",
+    demand: true,
     alias: "t",
-    }
+}
+
+const bodyOption = {
+    describe: "Show body",
+    demand: true,
+    alias: "b",
+}
+
 const argv = yargs
-.command("add" , "Add a new Note", {
- title : titleOptions,
- body : 
- { 
- describe : "Show body",
- demand :true,
- alias: "b",
- }
+    .command("add", "Add a new Note", {
+        title: titleOptions,
+        body: bodyOption
+    })
+
+.command("list", "List all the notes")
+
+.command("read", "Read any note", {
+    title: titleOptions
 })
-.command("list" , "List all the notes")
-.command("read" , "Read any note" ,{
- title : titleOptions
-})
- .command("remove" , "Remove any note" ,{
- title : titleOptions
-},
- )
-.help()
-.argv;
+
+.command("remove", "Remove any note", {
+        title: titleOptions
+    }, )
+    .help()
+    .argv;
+
 const command = yargs.argv._[0];
 let index = require('./index');
-if(command){
- if(command === "add") {
- let note = index.addNote(argv.title , argv.body)
- if(note){ 
- console.log("Note is Added")
- index.logNote(note)
-}else {
- console.log("You have already note of this title")
-}}
- else if(command === "read") {
- let note = index.readNote(argv.title)
- if(note){ 
- console.log("Your Note is here")
- index.logNote(note)
-}else {
- console.log("I cant find Note of this title")
-}}
 
- else if(command === "list"){
- let notes = index.listNotes()
- console.log(`Printing ${notes.length} Notes : `);
- notes.forEach(note => {
- index.logNote(note)
- });
- }
- else if(command === "remove") {
- const notes = index.removeNote(argv.title);
- console.log(notes);
- } 
- else console.log(`Your command ${command._[0]} not recognize`) 
+if (command) {
+    if (command === "add") {
+        let note = index.addNote(argv.title, argv.body)
+        if (note) {
+            console.log("Note is Added")
+            index.logNote(note)
+        } else {
+            console.log("You have already note of this title")
+        }
+    } else if (command === "read") {
+        let note = index.readNote(argv.title)
+        if (note) {
+            console.log("Your Note is here")
+            index.logNote(note)
+        } else {
+            console.log("I cant find Note of this title")
+        }
+    } else if (command === "list") {
+        let notes = index.listNotes()
+        console.log(`Printing ${notes.length} Notes : `);
+        notes.forEach(note => {
+            index.logNote(note)
+        });
+    } else if (command === "remove") {
+        const notes = index.removeNote(argv.title);
+        console.log(notes);
+    } else console.log(`Your command ${command._[0]} not recognize`)
 }
--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 let fs = require('fs');
 
 const fetchNotes = () => {
-    try{
+    try {
         const alreadyStringNotes = fs.readFileSync('notes.json')
-        return  JSON.parse(alreadyStringNotes);
-    }
-    catch(e){
+        return JSON.parse(alreadyStringNotes);
+    } catch (e) {
         return [];
     }
 }
 
 const saveNote = (notes) => {
-    fs.writeFileSync('notes.json' , JSON.stringify(notes));
+    fs.writeFileSync('notes.json', JSON.stringify(notes));
 }
-const addNote = (title , body) => {
-        let notes = fetchNotes();
-        const note = {
-            title, body
-        }
-        const filteredNote = notes.filter((note) => {return note.title === title})
-        if(filteredNote.length === 0){
-            notes.push(note);
-            saveNote(notes);
-            return note
-        }
+const addNote = (title, body) => {
+    let notes = fetchNotes();
+    const note = {
+        title,
+        body
     }
+    const filteredNote = notes.filter((note) => { return note.title === title })
+    if (filteredNote.length === 0) {
+        notes.push(note);
+        saveNote(notes);
+        return note
+    }
+}
 
 const readNote = (title) => {
-        const notes = fetchNotes();
-        const filteredNote = notes.filter((note) => note.title === title ? note : undefined)
-        return filteredNote[0]
+    const notes = fetchNotes();
+    const filteredNote = notes.filter((note) => note.title === title ? note : undefined)
+    return filteredNote[0]
 }
 
 const listNotes = () => {
@@ -38,12 +38,12 @@ const listNotes = () => {
 }
 
 const removeNote = (title) => {
-    if(title === true){
+    if (title === true) {
         console.log("Please add title");
-    return "" }
-    else {
+        return ""
+    } else {
         const notes = fetchNotes();
-        const filteredNote = notes.filter((note ) => note.title !== title);
+        const filteredNote = notes.filter((note) => note.title !== title);
         saveNote(filteredNote);
         let message = filteredNote.length !== notes.length ? "Note is removed" : "Note not found";
         return message
@@ -53,8 +53,8 @@ const removeNote = (title) => {
 const logNote = (note) => {
     console.log("-----");
     console.log(`Title : ${note.title}`)
-   console.log(`Body : ${note.body}`)
-   console.log("-----");
+    console.log(`Body : ${note.body}`)
+    console.log("-----");
 }
 module.exports = {
     addNote,
